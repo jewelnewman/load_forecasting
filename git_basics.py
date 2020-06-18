2a131173fe9d9daee1a76478e45cdbdf8bbc4e68
newmanjewel2@gmail.com <newmanjewel2@gmail.com>
2020-06-18 10:33:03 -0700

    Added py file with github notes

---
+++ git_basics.py
@@ -0,0 +1,39 @@
+# Important github commands and workflow
+
+'''
+Non git commands:
+
+pwd = displays present working directory path on local machine
+cd = change directory, cannot use backslash unless using " "
+    ex to change to C:\Users\jewel\SLAC do cd "C:\Users\jewel\SLAC" or
+    cd /c/Users/jewel/SLAC
+ls = lists files content in directory
+mv = moving a file or renaming file
+    ex mv pathtofile.ext differentpathtofile.ext
+    or mv oldfilename.ext newfilename.ext
+mkdir = make new folder
+    ex mkdir newfoldername
+touch = makes new files
+    ex touch filename.ext
+ctrl-l = clears screen
+
+'''
+'''
+Git commands:
+
+git status = list all changed files 
+
+git init = create new local repo in current directory or git init project name
+git clone = copies repo
+    ex copying repo from github git clone github url
+    or local repo git clone path to repo
+    
+git add = to add files to staging
+    ex git add filename.ext or git add . (select all files in current directory
+    or git add -A (select all files)
+git rm = removes file from repository
+    ex git rm --cached removes file from repo but keeps it in local machine
+git commit -m" "= saves staged changes to git directory
+
+git 
+
